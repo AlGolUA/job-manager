4ed1f7c6528b028314737110d56035961a7db14c
Alexander Golovko <alik@csltd.com.ua>
2022-01-16 20:25:57 +0200

    Меню второго уровня (администратора)

---
+++ server/src/main/webapp/src/app/admin/admin.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UsersComponent } from './users/users.component';
+import { OrdersComponent } from './orders/orders.component';
+
+@NgModule({
+  imports: [CommonModule,
+    RouterModule.forChild([
+      {path: '', component: AdminLayoutComponent, children: [
+          {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
+          {path: 'login', component: LoginPageComponent},
+          {path: 'users', component: UsersComponent},
+          {path: 'orders', component: OrdersComponent}
+        ]}
+    ])
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    UsersComponent,
+    OrdersComponent
+  ]
+})
+
+export class AdminModule{
+}
--- server/src/main/webapp/src/app/app-routing.module.ts
+++ server/src/main/webapp/src/app/app-routing.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainLayoutComponent} from "./shared/main-layout/main-layout.component";
 import {MainPageComponent} from "./shared/main-page/main-page.component";
-import {MainPageAdminComponent} from "./main-page-admin/main-page-admin.component";
 import {MainPageWorkerComponent} from "./main-page-worker/main-page-worker.component";
 import {MainPageOrgComponent} from "./main-page-org/main-page-org.component";
+import { AdminModule } from './admin/admin.module';
+import {MainPageAdminComponent} from "./main-page-admin/main-page-admin.component";
 
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
       {path: '', redirectTo: '/', pathMatch: 'full'},
       {path: '', component: MainPageComponent},
-      {path: 'admin', component: MainPageAdminComponent},
+      // {path: 'admin', component: MainPageAdminComponent},
       {path: 'worker', component: MainPageWorkerComponent},
       {path: 'org', component: MainPageOrgComponent}
     ]
+  },
+  {
+    // path: 'admin', loadChildren: './admin/admin.module.ts#AdminModule'
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(() => AdminModule)
   }
 ];
 
